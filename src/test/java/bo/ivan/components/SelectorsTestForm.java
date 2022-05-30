bd01127891cfcd02cd071d3d2d27fff44c890fea
IvanBo <aldeano3@gmail.com>
2022-05-31 01:59:56 +0300

    added SelectorsTestForm.java in components

---
+++ src/test/java/bo/ivan/components/SelectorsTestForm.java
@@ -0,0 +1,28 @@
+package bo.ivan.components;
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.$;
+
+public class SelectorsTestForm {
+    public SelenideElement
+            firstName = $("#firstName"),
+            lastName = $("#lastName"),
+            userEmail = $("#userEmail"),
+            gender = $("#genterWrapper"),
+            userNumber = $("#userNumber"),
+            subjectInput = $("#subjectsInput"),
+            hobbiesWrapper = $("#hobbiesWrapper"),
+            uploadPicture = $("#uploadPicture"),
+            currentAddress = $("#currentAddress"),
+            state = $("#state"),
+            city = $("#city"),
+            submitForm = $("#submit"),
+            titleResult = $("#example-modal-sizes-title-lg"),
+            tableResponsive = $("div.table-responsive"),
+            titlePracticeForm = $(".practice-form-wrapper");
+
+    public String
+            practiceFormTitle = "Student Registration Form",
+            resultTableTitle = "Thanks for submitting the form";
+}
