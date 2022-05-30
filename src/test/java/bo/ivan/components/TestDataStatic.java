8efac56a2accac7ddfbc90e5fb51ef273247fda6
IvanBo <aldeano3@gmail.com>
2022-05-31 01:56:52 +0300

    added TestDataStatic in components

---
+++ src/test/java/bo/ivan/components/TestDataStatic.java
@@ -0,0 +1,25 @@
+package bo.ivan.components;
+
+public class TestDataStatic {
+    public String firstName = "Ivan",
+                lastName = "Ivanov",
+                fullName = String.format("%s %s", firstName, lastName),
+                userEmail = "ivan@ivanov.su",
+                gender = "Male",
+                userNumber = "8919123456",
+                dayOfBirth = "07",
+                monthOfBirth = "May",
+                yearOfBirth = "1980",
+                dateOfBirth = String.format("%s %s,%s", dayOfBirth, monthOfBirth, yearOfBirth),
+                subjects = "Comp",
+                hobbies = "Reading",
+                picturePath = "./img/1.png",
+                currentAddress = "testing str., 20",
+                state = "Haryana",
+                city = "Karnal";
+
+    String[] picturePathArr = picturePath.split("/");
+        public String pictureName = picturePathArr[picturePathArr.length - 1];
+
+
+}
